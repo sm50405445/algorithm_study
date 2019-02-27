e0b89569dcc1e36221585ab264e597fa43b3e087
sd50712321 <sd50712321@naver.com>
2019-02-28 02:20:32 +0900

    SelectionSort

---
+++ Algorithm/src/com/algo/ex/SelectionSort.java
@@ -0,0 +1,28 @@
+package com.algo.ex;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		
+		int index=0, temp, min;
+		int[] array = {10,2,6,5,1,8,7,9,3,4};
+		
+		for(int i = 0;i<10;i++) {
+			min = 9999;
+			for(int j = i ; j<10;j++) {
+				if(array[j]<min) {
+					min = array[j];
+					index = j;
+				}
+			}
+			temp = array[i];
+			array[i] = array[index];
+			array[index] = temp;
+			
+		}
+		
+		for(int i = 0 ; i<array.length;i++) {
+			System.out.print(array[i]+",");
+		}
+	}
+}
