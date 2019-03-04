183b859b3d7df0a78ff3aae7ebd1f3f75c75b25c
sd50712321 <sd50712321@naver.com>
2019-03-05 00:59:24 +0900

    bubble, quick sort

--- Algorithm/src/com/algo/ex/SelectionSort.java
+++ Algorithm/src/com/algo/ex/SelectionSort.java
@@ -8,6 +8,7 @@ public class SelectionSort {
 		int[] array = {10,2,6,5,1,8,7,9,3,4};
 		
 		for(int i = 0;i<10;i++) {
+			//min 값을 여기서 초기화 해줘야 다음 번지부터 다시 비교 가능!
 			min = 9999;
 			for(int j = i ; j<10;j++) {
 				if(array[j]<min) {
